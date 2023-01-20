76cba28b3eaa8f27ddc9150306665ca38689e47d
maximiloi <maximiloi@gmail.com>
2023-01-20 13:16:11 +0300

    feat: half solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let OPEN_BRACKETS = [];
+    let BRACKETS_PAIR = {};
+    let stack = [];
+
+    bracketsConfig.forEach(([openSymbol, closeSymbol]) => {
+        OPEN_BRACKETS.push(openSymbol);
+        BRACKETS_PAIR[closeSymbol] = openSymbol;
+    });
+
+    for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+
+        if (OPEN_BRACKETS.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+
+            let topElement = stack[stack.length - 1];
+
+            if (BRACKETS_PAIR[currentSymbol] === topElement) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
